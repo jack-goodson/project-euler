551c5702b3f41bc3bd7974734176c2fe6d76f4b7
jack-goodson <32623308+jack-goodson@users.noreply.github.com>
2020-06-06 18:37:44 +1200

    Add files via upload

---
+++ Euler10/Euler10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Euler10
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            
+            long primeProduct = 0;
+            long prime = 2;
+            
+            while (prime < 2000000)
+            {
+                if (isPrime(prime))
+                {
+                    primeProduct += prime;
+                }
+
+                prime++;
+            }
+
+            Console.Write(primeProduct);
+        }
+        
+        private static bool isPrime(long n)
+        {
+            if (n <= 3)
+            {
+                return n > 1;
+            }else if (((n % 2) == 0) || ((n % 3) == 0))
+            {
+                return false;
+            }
+            int i = 5;
+            while ((i * i) <= n)
+            {
+                if (((n % i) == 0) || ((n % (i+2)) == 0))
+                {
+                    return false;
+                }
+                i += 6;
+            }
+            return true;
+        }
+    }
+}
